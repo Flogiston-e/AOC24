0113cda23d93e0302adf4f3b96019b01378c624e
Flogiston-e <sten.oskarson@gmail.com>
2024-12-10 06:54:09 +0100

    Add files via upload

--- AOC10.py
+++ AOC10.py
@@ -3,7 +3,7 @@ map = [[int(x)for x in list(row)] for row in map]
 
 paths = []
 pheads = []
-rating = []
+rating = 0
 index = 0
 H = len(map)
 W = len(map[0])
@@ -14,13 +14,12 @@ for i,row in enumerate(map):
             paths.append((i,j,0,index))
             pheads.append(set())
             index += 1
-            rating.append(0)
 
 while paths:
     loc = paths.pop(0)
     if loc[2] == 9:
         pheads[loc[3]].add((loc[0],loc[1]))
-        rating[loc[3]] += 1
+        rating += 1
     else:
         for dir in [(1,0),(0,1),(-1,0),(0,-1)]:
             if 0 <= loc[0]+dir[0] < H and 0 <= loc[1]+dir[1] < W:
@@ -28,4 +27,4 @@ while paths:
                     paths.append((loc[0]+dir[0], loc[1]+dir[1], loc[2]+1, loc[3]))
 
 print(sum([len(x) for x in pheads]))
-print(sum(rating))
+print(rating)
