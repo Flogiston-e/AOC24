6bbac1d0e77a8082a6cca8a7a6c33ad821371238
Flogiston-e <sten.oskarson@gmail.com>
2024-12-18 20:42:01 +0100

    Add files via upload

--- AOC17.py
+++ AOC17.py
@@ -3,6 +3,7 @@ input.pop(3)
 A, B, C, program = [x.split(":")[1] for x in input]
 A, B, C = int(A), int(B), int(C)
 program = [int(x) for x in program.split(",")]
+pl = len(program)
 
 def combo(A,B,C,operand):
     if operand < 4: return operand
@@ -10,10 +11,10 @@ def combo(A,B,C,operand):
     if operand == 5: return B
     if operand == 6: return C
 
-def computer(A,B,C,program):
+def computer(A,B,C,program,pl):
     output = []
     i = 0
-    while i < len(program)-1:
+    while i < pl-1:
         opcode, operand = program[i:i+2]
         if opcode == 0:
             A = A//(2**combo(A,B,C,operand))
@@ -35,29 +36,29 @@ def computer(A,B,C,program):
         i += 2
     return output
 
-print(computer(A,B,C,program))
+print(computer(A,B,C,program,pl))
 
-cycles = [8**i*7 for i in range(len(program)-2,-1,-1)] + [1]
+cycles = [8**i*7 for i in range(pl-2,-1,-1)] + [1]
 A = sum(cycles[1:-1])+1
 up = True
 ci = 0
-while ci < 16:
+while ci < pl:
     cycle = cycles[ci]
     while True:
         if up:
             A += cycle
         else:
             A -= cycle
-        output = computer(A,B,C,program)
+        output = computer(A,B,C,program,pl)
         if ci != 0:
-            if output[len(program)-ci] != program[len(program)-ci]:
+            if output[pl-ci] != program[pl-ci]:
                 if up:
                     ci -= 1
                     break
                 up = True
                 continue
         if up:
-            if output[len(program)-ci-1] == program[len(program)-ci-1]:
+            if output[pl-ci-1] == program[pl-ci-1]:
                 ci += 1
                 up = False
                 break
