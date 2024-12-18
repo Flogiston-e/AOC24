9613a92968b661f99dfc257d1f758e763ad638ea
Flogiston-e <sten.oskarson@gmail.com>
2024-12-18 19:22:34 +0100

    Add files via upload

--- AOC17.py
+++ AOC17.py
@@ -5,15 +5,10 @@ A, B, C = int(A), int(B), int(C)
 program = [int(x) for x in program.split(",")]
 
 def combo(A,B,C,operand):
-    if operand < 4:
-        return operand
-    if operand == 4:
-        return A
-    if operand == 5:
-        return B
-    if operand == 6:
-        return C
-    return False
+    if operand < 4: return operand
+    if operand == 4: return A
+    if operand == 5: return B
+    if operand == 6: return C
 
 def computer(A,B,C,program):
     output = []
@@ -42,11 +37,7 @@ def computer(A,B,C,program):
 
 print(computer(A,B,C,program))
 
-cycles = [8**i*7 for i in range(len(program)-1)]
-num_before = [0 for _ in range(len(program))]
-cycles.reverse()
-cycles.append(1)
-
+cycles = [8**i*7 for i in range(len(program)-2,-1,-1)] + [1]
 A = sum(cycles[1:-1])+1
 up = True
 ci = 0
@@ -59,16 +50,14 @@ while ci < 16:
             A -= cycle
         output = computer(A,B,C,program)
         if ci != 0:
-            if output[len(program)-ci] != num_before[ci-1]:
+            if output[len(program)-ci] != program[len(program)-ci]:
                 if up:
                     ci -= 1
-                    reset = True
                     break
                 up = True
                 continue
         if up:
             if output[len(program)-ci-1] == program[len(program)-ci-1]:
-                num_before[ci] = output[len(program)-ci-1]
                 ci += 1
                 up = False
                 break
