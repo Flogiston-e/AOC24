94caf3babbe62f3ae39be072d4414389731fcec4
Flogiston-e <sten.oskarson@gmail.com>
2024-12-18 15:03:20 +0100

    Add files via upload

--- AOC18.py
+++ AOC18.py
@@ -4,17 +4,17 @@ bytes = [(int(x),int(y)) for byte in input for x,y in [byte.split(",")]]
 def calculator(bytes,part1):
     Q, seen = [((0,0),0)], [(0,0)]
     while Q:
-        loc = Q.pop(0)
-        if loc[0] == (70,70):
-            if part1: return loc[1]
+        (x,y),steps = Q.pop(0)
+        if (x,y) == (70,70):
+            if part1: return steps
             return True
-        for d in [(1,0),(0,1),(-1,0),(0,-1)]:
-            if 0 <= loc[0][0]+d[0] <= 70 and 0 <= loc[0][1]+d[1] <= 70:
-                if (loc[0][0]+d[0], loc[0][1]+d[1]) not in seen+bytes:
-                    Q.append(((loc[0][0]+d[0], loc[0][1]+d[1]), loc[1]+1))
-                    seen.append((loc[0][0]+d[0], loc[0][1]+d[1]))
+        for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
+            if 0 <= x+dx <= 70 and 0 <= y+dy <= 70:
+                if (x+dx, y+dy) not in seen+bytes:
+                    Q.append(((x+dx, y+dy), steps+1))
+                    seen.append((x+dx, y+dy))
 
 print(calculator(bytes[0:1024],True))
-for i in range(len(bytes),0,-1):
+for i in range(len(bytes),-1,-1):
     if calculator(bytes[0:i],False):
         print(f"{bytes[i][0]},{bytes[i][1]}"); break
